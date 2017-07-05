c3172a19fd95d07209714503079d54d2c2fa7f3f
wpsaputra <wayanpermana93@gmail.com>
2017-07-05 18:03:58 +0700

    Add Piesql

--- views/site/index.php
+++ views/site/index.php
@@ -4,114 +4,66 @@
 use miloschuman\highcharts\Highcharts;
 
 $this->title = 'My Yii Application';
+$sql = "SELECT [status_dok] FROM [SOUT2017Sampel].[dbo].[t_rt_ternak] where [status_dok]='C'";
+$clean_ternak = Yii::$app->db->createCommand('SELECT COUNT(*) FROM (' . $sql . ') as count_alias')->queryScalar();
+
+$sql2 = "SELECT [status_dok] FROM [SOUT2017Sampel].[dbo].[t_rt_ternak] where [status_dok]='E'";
+$error_ternak = Yii::$app->db->createCommand('SELECT COUNT(*) FROM (' . $sql2 . ') as count_alias')->queryScalar();
+
+
+function printPie($judul, $clean, $error, $blm_entri)
+{
+    echo Highcharts::widget([
+        'options' => [
+            'title' => ['text' => $judul],
+            'credits' => ['enabled' => false],
+            'colors' => [
+                        // '#7cb5ec',
+                '#90ed7d',
+                '#f7a35c',
+                '#434348',
+            ],
+
+            'plotOptions' => [
+                'pie' => [
+                    'cursor' => 'pointer',
+                ],
+            ],
+            'series' => [
+                [ // new opening bracket
+
+
+
+                    'type' => 'pie',
+                    'name' => 'Elements',
+                    'data' => [
+                        ['Clean', $clean],
+                        ['Error', $error],
+                        ['Blm Entri', $blm_entri],
+                    ],
+                                        // 'dataLabels' => [
+                                        //     'enabled' => false
+                                        // ],
+                                        // 'showInLegend' => true
+                ] // new closing bracket
+            ],
+        ],
+    ]);
+
+}
 ?>
 <div class="site-index">
     <div class="body-content">
         <div class="row">
             <div class="col-lg-4">
-            <?php 
-            echo Highcharts::widget([
-                'options' => [
-                    'title' => ['text' => 'Progress Entri Padi'],
-                    'credits' => ['enabled' => false],
-                    'colors' => [
-                        // '#7cb5ec',
-                        '#90ed7d',
-                        '#f7a35c',
-                        '#434348',
-                    ],
-
-                    'plotOptions' => [
-                        'pie' => [
-                            'cursor' => 'pointer',
-                        ],
-                    ],
-                    'series' => [
-                        [ // new opening bracket
-
-
-                            'type' => 'pie',
-                            'name' => 'Elements',
-                            'data' => [
-                                ['Clean', 30],
-                                ['Error', 20],
-                                ['Blm Entri', 50],
-                            ],
-                                        // 'dataLabels' => [
-                                        //     'enabled' => false
-                                        // ],
-                                        // 'showInLegend' => true
-                        ] // new closing bracket
-                    ],
-                ],
-            ]);
-            
-            ?>
+                <?php printPie('Progress Entri Padi', 10, 20, 70); ?>
             </div>
             <div class="col-lg-4">
-            <?php 
-            echo Highcharts::widget([
-                'options' => [
-                    'credits' => ['enabled' => false],
-                    'title' => ['text' => 'Progress Entri Palawija'],
-                    'plotOptions' => [
-                        'pie' => [
-                            'cursor' => 'pointer',
-                        ],
-                    ],
-                    'series' => [
-                        [ // new opening bracket
-
-                            'type' => 'pie',
-                            'name' => 'Elements',
-                            'data' => [
-                                ['Firefox', 45.0],
-                                ['IE', 26.8],
-                                ['Safari', 8.5],
-                                ['Opera', 6.2],
-                                ['Others', 0.7]
-                            ],
-                        ] // new closing bracket
-                    ],
-                ],
-            ]);
-            ?>
+                <?php printPie('Progress Entri Palawija', 10, 20, 70); ?>
             </div>
             <div class="col-lg-4">
-            <?php 
-            echo Highcharts::widget([
-                'options' => [
-                    'credits' => ['enabled' => false],
-                    'title' => ['text' => 'Progress Entri Peternakan'],
-                    'plotOptions' => [
-                        'pie' => [
-                            'cursor' => 'pointer',
-                        ],
-                    ],
-                    'series' => [
-                        [ // new opening bracket
-
-                            'type' => 'pie',
-                            'name' => 'Elements',
-                            'data' => [
-                                ['Firefox', 45.0],
-                                ['IE', 26.8],
-                                ['Safari', 8.5],
-                                ['Opera', 6.2],
-                                ['Others', 0.7]
-                            ],
-                        ] // new closing bracket
-                    ],
-                ],
-            ]);
-            ?>
+                <?php printPie('Progress Entri Peternakan', 10, 20, 70); ?>
             </div>
-
-
-
-
-
-
 
         </div>
     </div>
